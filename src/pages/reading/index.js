371fde67b8ac28451a5a8b9eba73cc477c021dde
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-05-26 10:56:58 -0300

    effectively using chapter component and dynamically loading folder data via state variables

--- src/pages/reading/index.js
+++ src/pages/reading/index.js
@@ -2,21 +2,18 @@ import useIpfs from '../../hooks/useIpfs.js';
 import {useEffect, useState} from 'react';
 import React from 'react';
 
+import Chapter from '../../components/chapter'
 
 export default function Reading(){
   const { ipfs, ipfsState } = useIpfs()
   const [data, setData] = useState([])
   const [nodeInfo, setNodeInfo]  = useState()
-  const [topic, setTopic] = useState('')
+  const [CID, setCID] = useState()
+  const [formInput, setFormInput] = useState()
 
-
-  async function setupListener(event){
+  function updateFolder(event){
     event.preventDefault()
-    
-    ipfs.pubsub.subscribe(topic, msg => {
-      console.log(msg)
-      setData(oldData => [ ...oldData, msg.data.toString()])
-    })
+    setCID(formInput)
   }
 
   useEffect(() => {
@@ -40,30 +37,13 @@ export default function Reading(){
 
   return(
     <>
-    {ipfsState && nodeInfo ? `Node set with id ${nodeInfo.id}` : 'Node not set yet'}
+      {ipfsState && nodeInfo ? `Node set with id ${nodeInfo.id}` : 'Node not set yet'}
+      <form onSubmit={updateFolder}>
+        <input type='text' placeholder='Insert folder CID' onChange={event => setFormInput(event.target.value)}/>
+        <button type='submit'>Get files</button>
+      </form>
+      <Chapter ipfs={ipfs} cid={CID}/>
 
-    <form onSubmit={setupListener}>
-      <label htmlFor='topicForm'>Tópico</label>
-      <input 
-        id='topicForm'
-        type='text'
-        placeholder='legosi feet pics'
-        onChange={event => setTopic(event.target.value)}
-      />
-      <button type='submit'>Inscrever</button>
-    </form>
-    {data.map((msg,index) => {
-      if( index === data.length - 1 ) return (
-        <>
-        <h3>Nova mensagem!</h3>
-        <p>{msg}</p>
-        </>
-      )
-
-      return (
-        <p>{msg}</p>
-      )
-    })}
     </>
   )
 }
