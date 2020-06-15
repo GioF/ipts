0c861f7ab84b8ad1e67d3a2444ded3e9aeb84961
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-06-15 18:35:31 -0300

    Finally using the container for something

--- src/pages/reading/styles.js
+++ src/pages/reading/styles.js
@@ -4,4 +4,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 100%;
   width: 100%;
+  
+  display: flex;
+  flex-direction: row
+  justify-content: space-between;
 `;
