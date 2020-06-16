c494ca613b3652c2a98fabf2c8e15d1a6146211f
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-06-16 19:39:03 -0300

    better styled the ipfs folder input

--- src/pages/reading/index.js
+++ src/pages/reading/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from "react";
 import React from "react";
-import { Container } from "./styles";
+import { Container, FolderInput } from "./styles";
 
 import Chapter from "./chapter";
 import Dock from "./dock";
@@ -8,25 +8,33 @@ import { IpfsContext } from "../../App";
 
 export default function Reading() {
   const { ipfs, ipfsState } = useContext(IpfsContext);
-  const [CID, setCID] = useState();
+  const [cid, setCid] = useState();
   const [formInput, setFormInput] = useState();
 
   function updateFolder(event) {
     event.preventDefault();
-    setCID(formInput);
+    setCid(formInput);
   }
 
   return (
     <Container>
-      <form onSubmit={updateFolder}>
-        <input
-          type="text"
-          placeholder="Insert folder CID"
-          onChange={(event) => setFormInput(event.target.value)}
-        />
-        <button type="submit">Get files</button>
-      </form>
-      <Chapter ipfs={ipfs} cid={CID} />
+      {cid ? (
+        <>
+          <Chapter ipfs={ipfs} cid={cid} />
+        </>
+      ) : (
+        <form onSubmit={updateFolder}>
+          <FolderInput
+            style={{
+              backgroundImage: `url(${require("../../res/ipfsindicator.svg")})`,
+            }}
+            type="text"
+            placeholder="Insert folder CID"
+            onChange={(event) => setFormInput(event.target.value)}
+          />
+          <button type="hidden" />
+        </form>
+      )}
       <Dock width={"300"}>{/*TODO: add list components*/}</Dock>
     </Container>
   );
--- src/pages/reading/styles.js
+++ src/pages/reading/styles.js
@@ -1,11 +1,30 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
   height: 100%;
   width: 100%;
-  
+
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  form {
+    display: block;
+    margin: auto;
+  }
+`;
+
+export const FolderInput = styled.input`
+  height: 40px;
+  width: 500;
+
+  background-color: #4e5761;
+  border-radius: 11px;
+  padding: 6px 15px 6px 46px;
+  resize: none;
+
+  font-size: 24px;
+  color: #282828;
+  background-position: 10px 4px;
+  background-repeat: no-repeat;
 `;
