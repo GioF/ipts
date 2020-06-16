a38e503fa4bb487c732b00de0b46a6de595888d7
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-06-16 17:53:26 -0300

    moving additional dev node connection to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Reading from "./pages/reading/index.js";
 import Sidebar from "./components/sidebar/index.js";
 import useIpfs from "./hooks/useIpfs";
@@ -9,6 +9,17 @@ export const IpfsContext = React.createContext({});
 
 function App() {
   const { ipfs, ipfsState } = useIpfs();
+  useEffect(() => {
+    if (ipfsState) {
+      if (process.env.NODE_ENV === "development") {
+        ipfs.swarm
+          .connect(process.env.REACT_APP_IPFS_MULTIADDR)
+          .then(console.log("connected to dev node"))
+          .catch(console.log);
+      }
+    }
+  }, [ipfs, ipfsState]);
+
   return (
     <div className="App">
       <IpfsContext.Provider value={{ ipfs, ipfsState }}>
--- src/pages/reading/index.js
+++ src/pages/reading/index.js
@@ -8,7 +8,6 @@ import { IpfsContext } from "../../App";
 
 export default function Reading() {
   const { ipfs, ipfsState } = useContext(IpfsContext);
-  const [nodeInfo, setNodeInfo] = useState();
   const [CID, setCID] = useState();
   const [formInput, setFormInput] = useState();
 
@@ -17,24 +16,8 @@ export default function Reading() {
     setCID(formInput);
   }
 
-  useEffect(() => {
-    if (ipfsState) {
-      ipfs.id().then(setNodeInfo).catch(console.log);
-
-      if (process.env.NODE_ENV === "development") {
-        ipfs.swarm
-          .connect(process.env.REACT_APP_IPFS_MULTIADDR)
-          .then(console.log("connected to dev node"))
-          .catch(console.log);
-      }
-    }
-  }, [ipfs, ipfsState]);
-
   return (
     <Container>
-      {ipfsState && nodeInfo
-        ? `Node set with id ${nodeInfo.id}`
-        : "Node not set yet"}
       <form onSubmit={updateFolder}>
         <input
           type="text"
