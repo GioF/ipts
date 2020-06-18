09a0095f9daaa295c6456b7c9ca61e68cf1c23e7
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-06-18 01:52:50 -0300

    refactoring

--- src/pages/reading/page/index.js
+++ src/pages/reading/page/index.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import {Buffer} from 'buffer';
-import {Box, Container} from './styles';
+import React from "react";
+import { useEffect, useState } from "react";
+import { Buffer } from "buffer";
+import { Box, Container } from "./styles";
 
-
-export default function Page({data, image, ipfs}){
-  const [imgBuffer, setImgBuffer] = useState([])
-  const [translation, setTranslation] = useState()
-  const [controller] = useState(new AbortController())
-
+export default function Page({ data, image, ipfs }) {
+  const [imgBuffer, setImgBuffer] = useState([]);
+  const [translation, setTranslation] = useState();
+  const [controller] = useState(new AbortController());
 
   useEffect(() => {
-    //starts to load image then translation from given cid into buffer and aborts  
+    //starts to load image then translation from given cid into buffer and aborts
     //via controller if component will unmout
-    async function handleStream(){
-      const signal = controller.signal
-      for await(const chunk of ipfs.cat(image.cid, { signal })){
-        setImgBuffer(oldBuff => [...oldBuff, chunk]) 
+    async function handleStream() {
+      const signal = controller.signal;
+      for await (const chunk of ipfs.cat(image.cid, { signal })) {
+        setImgBuffer((oldBuff) => [...oldBuff, chunk]);
       }
 
-      let textBuffer = []
+      let textBuffer = [];
       //will only begin loading translation data if component isn't unloading
-      for await(const chunk of ipfs.cat(data.cid, { signal })){
-        textBuffer = [...textBuffer, chunk]
+      for await (const chunk of ipfs.cat(data.cid, { signal })) {
+        textBuffer = [...textBuffer, chunk];
       }
-      setTranslation(JSON.parse(Buffer.concat(textBuffer).toString()))
+      setTranslation(JSON.parse(Buffer.concat(textBuffer).toString()));
     }
 
-    if(ipfs && data) handleStream()
+    if (ipfs && data) handleStream();
 
-    return function cleanup () {
-      controller.abort()
-      setImgBuffer([])
-    }
-  }, [data, image, ipfs])
+    return function cleanup() {
+      controller.abort();
+      setImgBuffer([]);
+    };
+  }, [data, image, ipfs]);
 
   return (
     <Container>
-      <img src={`data:image/jpg;base64,${Buffer.concat(imgBuffer).toString('base64')}`}/>
-      {translation ? translation.map( (props, index) => {
-        return <Box key={index} { ...props } title={props.text} />
-      }) : null}
+      <img
+        src={`data:image/jpg;base64,${Buffer.concat(imgBuffer).toString(
+          "base64"
+        )}`}
+      />
+      {translation
+        ? translation.map((props, index) => {
+            return <Box key={index} {...props} title={props.text} />;
+          })
+        : null}
     </Container>
-  )
+  );
 }
